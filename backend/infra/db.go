b4f3210f5adcc4f4ecc1ed09fa334dde3910ab7b
Kai Kota <kai.kota1027@gmail.com>
2025-11-26 14:14:03 +0900

    docker composeでデータベース構築

---
+++ backend/infra/db.go
@@ -0,0 +1,37 @@
+package infra
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func SetupDB() *gorm.DB {
+	env := os.Getenv("ENV")
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tokyo",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
+	var (
+		db  *gorm.DB
+		err error
+	)
+
+	if env == "prod" {
+		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		log.Println("Setup postgresql database")
+	}
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	return db
+
+}
--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"chatapp/backend/infra"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func setupRouter(db *gorm.DB) *gin.Engine {
+	router := gin.Default()
+	// ルートやハンドラの設定をここに追加
+	return router
+}
 
 func main() {
-	router := gin.Default();
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	router.Run() // listen and serve on
+	//インフラの構築
+	infra.Initialize()
+	db := infra.SetupDB()
+	router := setupRouter(db)
+
+	router.Run(":8080")
 }

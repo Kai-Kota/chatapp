c5b393690d4bda1800395b1b35e7d945f05d4ac0
Kai Kota <kai.kota1027@gmail.com>
2025-12-03 01:33:22 +0900

    ユーザーと部屋のmany2many実装(途中)

--- backend/services/room_service.go
+++ backend/services/room_service.go
@@ -19,14 +19,27 @@ func NewRoomService(repository repositories.IRoomRepository) IRoomService {
 }
 
 func (s *RoomService) Create(userId uint, pertner string) (*models.Room, error) {
-	pertnerId := s.repository.FindUserIdByName(pertner)
 
 	// 新しいチャットルームの初期データを設定
 	newRoom := models.Room{
-		
+		Name: "Room_" + "_" + pertner,
 		//Messages: []models.Message{}, // 初期状態ではメッセージは空
 	}
-	return s.repository.Create(newRoom)
+
+	partnerId := s.repository.FindUserIdByName(pertner)
+
+	// チャットルームの作成
+	room, err := s.repository.Create(newRoom)
+
+	// ユーザーとチャットルームの関連付け
+	if err := s.repository.AssoiciateUserToRoom(userId, newRoom); err != nil {
+		return nil, err
+	}
+	if err := s.repository.AssoiciateUserToRoom(partnerId, newRoom); err != nil {
+		return nil, err
+	}
+
+	return room, err
 }
 
 func (s *RoomService) GetUserRooms(userId uint) (*[]models.Room, error) {
